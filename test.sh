bde84222fefddeeae263c120c6f96271f82154ad
david_smith <david_smith@sweetwater.com>
2025-01-06 02:05:02 -0500

    Update tests.

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ services=("8.4" "8.3" "8.2" "8.1" "8.0" "7.4" "7.3" "7.2" "7.1")
 for service in "${services[@]}"; do
   docker compose run --rm "php${service}composer" composer update --no-cache
 
-  if ! docker compose run --rm "php${service}" vendor/bin/phpunit --configuration "phpunit${service}.xml"; then
+  if ! docker compose run --rm "php${service}" vendor/bin/phpunit --configuration docker/php/"${service}"/phpunit.xml; then
     exit 1
   fi
 done
