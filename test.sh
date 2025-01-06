50d6307fe2de955d8e5c9fedcdcfca006ffbf16b
david_smith <david_smith@sweetwater.com>
2025-01-06 09:09:18 -0500

    Update tests.

--- test.sh
+++ test.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-services=("8.4" "8.3" "8.2" "8.1" "8.0" "7.4" "7.3" "7.2" "7.1")
+PHP_VERSIONS=("8.4" "8.3" "8.2" "8.1" "8.0" "7.4" "7.3" "7.2" "7.1")
 
-for service in "${services[@]}"; do
-  docker compose run --rm "php${service}composer" composer update --no-cache
+for version in "${PHP_VERSIONS[@]}"; do
 
-  if ! docker compose run --rm "php${service}" vendor/bin/phpunit --configuration docker/php/"${service}"/phpunit.xml; then
+  export PHP_COMPOSER="${version}"
+  export PHP_VERSION="${version}"
+
+  docker compose run --rm composer composer update --no-cache
+
+  if ! docker compose run --rm php vendor/bin/phpunit --configuration "docker/php/${version}/phpunit.xml"
+  then
     exit 1
   fi
 done
