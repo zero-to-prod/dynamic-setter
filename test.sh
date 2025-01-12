7b8ccacc03cf40dc7c87e94dd387698c42f1a5ca
david_smith <david_smith@sweetwater.com>
2025-01-12 05:31:21 -0500

    maint/update-dock Updated `test.sh`.

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@ set -e
 
 PHP_VERSIONS=("8.4" "8.3" "8.2" "8.1" "8.0" "7.4" "7.3" "7.2" "7.1")
 
-for version in "${PHP_VERSIONS[@]}"; do
+for PHP_VERSION in "${PHP_VERSIONS[@]}"; do
 
-  docker compose run --rm "composer${version}" composer update
+  sh dock "composer${PHP_VERSION}" composer update
 
-  if ! docker compose run --rm "php${version}" ./vendor/bin/phpunit
+  if ! sh dock "php${PHP_VERSION}" ./vendor/bin/phpunit
   then
     exit 1
   fi
